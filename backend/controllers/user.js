925daac6b88f120c0a5a68ec245c71220b81a75d
Rundan53 <rundanonkar1999@gmail.com>
2023-12-20 22:56:13 +0530

    added login functionalities

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,21 +1,59 @@
 const User = require('../models/User');
 
-exports.postUser = (req, res)=> {
-    const userToAdd = req.body;
+function isStringValidate(string) {
+    if(string.length===0 || !string){
+        return false;
+    }
+    return true;
+}
 
-    User.findOne({where: 
-        {email: userToAdd.email}
-    })
+exports.signUp = (req, res)=> {
+    const {username, email, password} = req.body;
+
+    if(!isStringValidate(username) || !isStringValidate(email) || !isStringValidate(password)){
+        return res.status(400).json({error: 'Bad parameters: Something is missing'})
+    }
+
+    User.findOne({where: {email: email}})
     .then((user)=> {
 
         if(user) {
             //if user already exist, send 409 conflict response
-            return res.status(409).json({error: 'user already exist'})
+            return res.status(409).json({error: 'User already exist'})
         }
 
-        User.create(userToAdd)
+        User.create({username, email, password})
         .then(()=>{
-            res.status(201).json({message:'User Created Successfully'})
+            res.status(201).json({message:'User Created Successfully'});
+        })
+    })
+    .catch((err)=> {
+        res.status(500).json({ error: 'Internal Server Error' });
+    })
+}
+
+
+
+exports.login = (req, res)=> {
+    const {email, password} = req.body;
+    console.log(email,password)
+    if( !isStringValidate(email) || !isStringValidate(password)){
+        return res.status(400).json({error: 'Bad parameters: Something is missing'})
+    }
+
+    User.findOne({where: {email: email}})
+    .then((user)=> {
+        if(!user) {
+            return res.status(404).json({error: 'Wrong Email or password'})
+        }
+
+        User.findOne({where: {password: password}})
+        .then((user)=> {
+            if(!user) {
+                return res.status(404).json({error: 'Wrong Email or password'});
+            }
+
+            res.status(201).json('Successfully login');
         })
     })
     .catch((err)=> {
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,26 +1,43 @@
-function postUser(e) {
+function signUp(e) {
     e.preventDefault();
 
-    const username = e.target.username.value;
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+    const { username, email, password } = e.target;
 
     const signupDetails = {
-        username: username,
-        email: email,
-        password: password,
+        username: username.value,
+        email: email.value,
+        password: password.value,
     }
 
     axios.post(`http://localhost:3000/user/sign-up`, signupDetails)
-    .then((res)=> {
-        console.log(res.data);
-        if(res.status == 201) {
-            window.location.href = '../login/login.html'
-        }
-        else{
-            console.log(res);
-        }
-    })
-    .catch(err=> alert(err.response.data.error) )
+        .then((res) => {
+            console.log(res.data);
+            if (res.status == 201) {
+                window.location.href = './login.html'
+            }
+            else {
+                throw new Error('failed to login')
+            }
+        })
+        .catch(err => {
+           handleError(e.target,err.response.data.error);
+        })
 
+}
+
+
+function handleError(target,error) {
+    const existingErrorMessages = target.querySelectorAll('p');
+    existingErrorMessages.forEach((errMessage) => {
+        if(errMessage.id === 'errorMessage'){
+            errMessage.remove();
+        }
+    });
+    
+    let errMessage = document.createElement('p');
+    errMessage.id = 'errorMessage';
+    errMessage.innerHTML = error;
+    errMessage.style.color = 'red';
+    errMessage.style.textDecoration = 'underline'
+    target.append(errMessage);
 }
